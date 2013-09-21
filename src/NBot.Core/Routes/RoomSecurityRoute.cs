57773c7be846c7f418b0c55a61dcb10bb6f68409
jonathan.starnes <jonathanstarnes@gmail.com>
2013-09-21 01:30:28 -0500

    Added room based security closes #2

---
+++ src/NBot.Core/Routes/RoomSecurityRoute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace NBot.Core.Routes
+{
+    public class RoomSecurityRoute : IRoute
+    {
+        private readonly IRoute _innerRoute;
+        private readonly List<string> _allowedRooms;
+
+        public RoomSecurityRoute(IRoute innerRoute, List<string> allowedRooms)
+        {
+            _innerRoute = innerRoute;
+            _allowedRooms = allowedRooms;
+        }
+
+        public IMessageHandler Handler { get { return _innerRoute.Handler; } }
+        public MethodInfo EndPoint { get { return _innerRoute.EndPoint; } }
+
+        public bool IsMatch(Message message)
+        {
+            return _innerRoute.IsMatch(message) && _allowedRooms.Contains(message.RoomId);
+        }
+    }
+}

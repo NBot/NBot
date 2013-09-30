2649765ad5405c3b9d009880b9db4dccfca0bdaf
jstarnes <jonathanstarnes@gmail.com>
2013-09-30 00:54:22 -0500

    Fixed a merge issue

--- src/NBot.ConsoleAdapter/Program.cs
+++ src/NBot.ConsoleAdapter/Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Reflection;
+using System.Reflection;
 using NBot.Core;
 using NBot.Core.Brains;
 using NBot.Core.Messaging.ContentFilters;
@@ -22,29 +21,4 @@ namespace NBot.ConsoleAdapter
                 .Run(); // <- Get Crackin
         }
     }
-=======
-﻿using System.Reflection;
-using NBot.Core;
-using NBot.Core.Brains;
-using NBot.Core.Messaging.ContentFilters;
-
-namespace NBot.ConsoleAdapter
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            // New up a brain to use
-            var brain = new FileBrain(".\\Brain");
-
-            Robot.Create("NBot")
-                .UseBrain(brain) // <- Use your brain
-                .RegisterMessageFilter(new HandleBarsMessageFilter(brain)) // <- Register zero or more Message Filters
-                .RegisterAdapter(new ConsoleAdapter(), "ConsoleChannel") // <- Register one ore more Adapters
-                .RegisterHandlersInAssembly(Assembly.Load("NBot.MessageHandlers")) // <- Register all the Handlers
-                .AllowedInAllRooms() // <- Allow them in all rooms
-                .Run(); // <- Get Crackin
-        }
-    }
->>>>>>> 09ce144... Fixed program.cs
 }

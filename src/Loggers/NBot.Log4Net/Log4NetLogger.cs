45f9213cb1d7463d0d124f2042b22123e8e3ba31
Jonathan Starnes <jonathan.starnes@gmail.com>
2014-07-17 23:20:30 -0500

    Merged NBot.Log4Net into NBot resolves #51

---
+++ src/Loggers/NBot.Log4Net/Log4NetLogger.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using log4net.Config;
+using NBot.Core.Logging;
+using System;
+
+namespace NBot.Log4Net
+{
+    public class Log4NetLogger : INBotLog
+    {
+        private readonly ILog _log;
+        public Log4NetLogger(string name)
+        {
+            XmlConfigurator.Configure();
+            _log = LogManager.GetLogger(name);
+        }
+
+        public void WriteInfo(object message, Exception exception = null)
+        {
+            _log.Info(message, exception);
+        }
+
+        public void WriteDebug(object message, Exception exception = null)
+        {
+            _log.Debug(message, exception);
+        }
+
+        public void WriteWarning(object message, Exception exception = null)
+        {
+            _log.Warn(message, exception);
+        }
+
+        public void WriteError(object message, Exception exception = null)
+        {
+            _log.Error(message, exception);
+        }
+
+        public void WriteFatal(object message, Exception exception = null)
+        {
+            _log.Fatal(message, exception);
+        }
+    }
+}
---
+++ src/Loggers/NBot.Log4Net/Log4NetRobotExtensions.cs
@@ -0,0 +1,11 @@
+﻿using NBot.Log4Net;
+namespace NBot.Core
+{
+    public static class RobotExtensions
+    {
+        public static IRobotConfiguration UseLog4Net(this IRobotConfiguration target, string name)
+        {
+            return target.UseLog(new Log4NetLogger(name));
+        }
+    }
+}

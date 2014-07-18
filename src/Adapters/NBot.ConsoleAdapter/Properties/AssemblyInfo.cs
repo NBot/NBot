93d60c6ba1d3fb81ec0fe4f7110f8f7b60440079
Jonathan Starnes <jonathan.starnes@gmail.com>
2014-07-17 20:51:55 -0500

    Restructured Project reolves #48

--- src/Adapters/NBot.ConsoleAdapter/Properties/AssemblyInfo.cs
+++ src/Adapters/NBot.ConsoleAdapter/Properties/AssemblyInfo.cs
@@ -1,28 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("NBot.ConsoleAdapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NBot.ConsoleAdapter")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("685c674d-e803-4b31-80ba-fbd8098c1c64")]
+[assembly: Guid("5dcde804-8531-4254-bb3b-356bdef345dc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Brains/NBot.FileBrain/RobotExtensions.cs
@@ -0,0 +1,18 @@
+﻿using NBot.Core;
+using NBot.Core.Brains;
+
+namespace NBot
+{
+    public static class RobotExtensions
+    {
+        public static IRobotConfiguration UseFileBrain(this IRobotConfiguration configuration)
+        {
+            return configuration.UseFileBrain(".\\Brain");
+        }
+
+        public static IRobotConfiguration UseFileBrain(this IRobotConfiguration configuration, string filePath)
+        {
+            return configuration.UseBrain(new FileBrain(filePath));
+        }
+    }
+}
